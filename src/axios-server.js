4262e9b5f52c47326a5f3483dba6eb139e764ef9
hrugved <whrugved@gmail.com>
2022-04-02 21:40:21 +0530

    change axios url

--- src/axios-server.js
+++ src/axios-server.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     // baseURL:'http://localhost:3000'
-    baseURL:'http://ec2-35-175-197-64.compute-1.amazonaws.com/api'
+    baseURL:'http://ec2-3-80-215-110.compute-1.amazonaws.com/api'
 });
 
 export default instance;
