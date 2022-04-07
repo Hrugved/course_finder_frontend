439658acd5980c0cb67fbfad81621429b71fee06
hrugved <whrugved@gmail.com>
2022-04-08 05:11:10 +0530

    change url

--- src/axios-server.js
+++ src/axios-server.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     // baseURL:'http://localhost:3000'
-    baseURL:'http://ec2-54-165-91-173.compute-1.amazonaws.com/api'
+    baseURL:'http://http://courses-navigator.herokuapp.com'
 });
 
 export default instance;
